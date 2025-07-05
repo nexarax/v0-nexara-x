17b8039ccb9b0a22f4b59074f881773d64174027
nexarax <jason@nexarax.com>
2025-07-05 19:18:10 +0100

    favicon.ico 0507 1917

--- app/favicon.ico/route.ts
+++ app/favicon.ico/route.ts
@@ -1,6 +1,6 @@
-import { type NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   // Redirect favicon.ico requests to your PNG icon
-  return NextResponse.redirect(new URL("/icon-192.png", request.url))
+  return NextResponse.redirect(new URL("/icon-192.png", "https://nexarax.com"), 301)
 }
